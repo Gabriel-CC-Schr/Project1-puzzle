7e07c54cf522af675caaa231e53a189f6268ed1d
Gabriel CC <gabriel.pizza96@gmail.com>
2026-09-15 14:35:43 -0700

    push

--- solvingpuzzle.cpp
+++ solvingpuzzle.cpp
@@ -1,1 +1,10 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+ofstream result("./result", ios::out);
+
+ifstream puzzle("./puzzle.txt", ios::in);
