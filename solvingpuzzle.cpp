82f4e74d114d3f50d058312e0f7ee6c3dcf21fca
Gabriel CC <gabriel.pizza96@gmail.com>
2026-09-17 17:34:10 -0700

    reading the file

--- solvingpuzzle.cpp
+++ solvingpuzzle.cpp
@@ -5,10 +5,40 @@
 
 using namespace std;
 
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        cerr << "Usage: " << argv[0] << " <filename>\n";
+        return 1;
+    }
 
+    const string inputPuzzleFile = string(argv[1]);
 
-ofstream result("./result", ios::out);
+    // Check if the input file exists and is readable
+    ifstream inputFile(inputPuzzleFile);
+    if (!inputFile)
+    {
+        cerr << "Error: Cannot open file " << inputPuzzleFile << endl;
+        return 1;
+    }
 
-ifstream puzzle("./puzzle.txt", ios::in);
+    vector<string> words;
+    string word;
 
-static const vector<vector<int>> puzzle_data;
+    // Read words from the input file
+    while (inputFile >> word)
+    {
+        words.push_back(word);
+    }
+    inputFile.close();
+
+    // Output the words to the console
+    cout << "Words read from file:\n";
+    for (const auto &w : words)
+    {
+        cout << w << endl;
+    }
+
+    return 0;
+}
